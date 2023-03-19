141d3a6660c78e424137cd54883371bbbda516b4
Felix Fontein <felix@fontein.de>
2023-03-19 15:35:39 +0100

    DO NOT MERGE - foo

--- src/ansible.ts
+++ src/ansible.ts
@@ -12,3 +12,7 @@ export function isPluginType(input: string): boolean {
   /* We do not want to hard-code a list of valid plugin types that might be inaccurate, so we simply check whether this is a valid kind of Python identifier usually used for plugin types. If ansible-core ever adds one with digits, we'll have to update this. */
   return /^[a-z_]+$/.test(input);
 }
+
+export function foo(): boolean {
+  return false;
+}
