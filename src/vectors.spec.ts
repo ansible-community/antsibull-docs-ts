4e4f72945850b281681ba76c7cfa4a89d7346805
Felix Fontein <felix@fontein.de>
2024-08-04 16:34:52 +0200

    Tests: use templates instead of code evaluation (#285)
    * Use templates instead of code evaluation.
    
    * Lint.

--- src/vectors.spec.ts
+++ src/vectors.spec.ts
@@ -14,26 +14,41 @@ import { toMD } from './md';
 import { toRST } from './rst';
 import { toAnsibleDocText } from './ansible-doc-text';
 
+function parseLinkProvider(opts) {
+  if (opts['pluginLinkTemplate']) {
+    const template = opts['pluginLinkTemplate'];
+    opts.pluginLink = (plugin_data) =>
+      template
+        .replace(/{plugin_fqcn}/g, plugin_data.fqcn)
+        .replace(/{plugin_fqcn_slashes}/g, plugin_data.fqcn.replace(/\./g, '/'))
+        .replace(/{plugin_type}/g, plugin_data.type);
+  }
+  if (opts['pluginOptionLikeLinkTemplate']) {
+    const template = opts['pluginOptionLikeLinkTemplate'];
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    opts.pluginOptionLikeLink = (plugin, entrypoint, what, name, current_plugin) =>
+      template
+        .replace(/{plugin_fqcn}/g, plugin.fqcn)
+        .replace(/{plugin_fqcn_slashes}/g, plugin.fqcn.replace(/\./g, '/'))
+        .replace(/{plugin_type}/g, plugin.type)
+        .replace(/{what}/g, what)
+        .replace(/{entrypoint}/g, entrypoint || '')
+        .replace(/{entrypoint_with_leading_dash}/g, entrypoint ? '-' + entrypoint : '')
+        .replace(/{name_dots}/g, name.join('.'))
+        .replace(/{name_slashes}/g, name.join('/'));
+  }
+}
+
 describe('vectors', (): void => {
   const data = readFileSync('test-vectors.yaml', 'utf8');
   const vectors = parse_yaml(data).test_vectors;
   for (const test_name of Object.keys(vectors)) {
     const test_data = vectors[test_name];
     if (test_data.html_opts) {
-      if (test_data.html_opts['pluginLink.js']) {
-        test_data.html_opts.pluginLink = eval(test_data.html_opts['pluginLink.js']);
-      }
-      if (test_data.html_opts['pluginOptionLikeLink.js']) {
-        test_data.html_opts.pluginOptionLikeLink = eval(test_data.html_opts['pluginOptionLikeLink.js']);
-      }
+      parseLinkProvider(test_data.html_opts);
     }
     if (test_data.md_opts) {
-      if (test_data.md_opts['pluginLink.js']) {
-        test_data.md_opts.pluginLink = eval(test_data.md_opts['pluginLink.js']);
-      }
-      if (test_data.md_opts['pluginOptionLikeLink.js']) {
-        test_data.md_opts.pluginOptionLikeLink = eval(test_data.md_opts['pluginOptionLikeLink.js']);
-      }
+      parseLinkProvider(test_data.md_opts);
     }
     if (test_data.source !== undefined && test_data.html !== undefined) {
       it(`${test_name} (Ansible doc => HTML)`, (): void => {
